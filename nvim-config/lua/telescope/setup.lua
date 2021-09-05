056199e03ec4a228b80561a02670ff98d1b64c97
teleivo <teleivo@users.noreply.github.com>
2021-09-05 10:42:44 +0200

    nvim: clean up

--- nvim-config/lua/telescope/setup.lua
+++ nvim-config/lua/telescope/setup.lua
@@ -1,17 +1,13 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
 require('telescope').setup {
-  defaults = {
-      disable_devicons = false,
-  },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
     }
   }
 }
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
+-- To get fzf loaded and working with telescope
 require('telescope').load_extension('fzf')
