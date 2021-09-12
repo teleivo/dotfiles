e1082b2696b6df1ee843d668dd758a856cdc3f73
teleivo <teleivo@users.noreply.github.com>
2021-09-12 06:08:09 +0200

    nvim: try different treesitter selection keymaps

--- nvim-config/lua/treesitter/setup.lua
+++ nvim-config/lua/treesitter/setup.lua
@@ -20,10 +20,10 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
+      init_selection = 'gh',
+      node_incremental = 'gh',
+      scope_incremental = 'gH',
+      node_decremental = 'gl',
     },
   },
   indent = {
