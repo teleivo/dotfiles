57191ac689889479707ce838dcf8072921407081
teleivo <teleivo@users.noreply.github.com>
2021-09-16 12:49:07 +0200

    nvim: extract LSP mappings
    so I can reuse them in the Java LSP config which is separate
    from the lspconfig one

--- nvim-config/lua/my/lsp/init.lua
+++ nvim-config/lua/my/lsp/init.lua
@@ -1,32 +1,37 @@
 local nvim_lsp = require('lspconfig')
-local on_attach = function(_, bufnr)
+
+-- find out what an LSP can with
+-- lua print(vim.inspect(vim.lsp.protocol.make_client_capabilities())
+local key_mappings = {
+  {'n', 'gq', '<cmd>lua vim.lsp.buf.formatting()<cr>'},
+  -- range formatting does not seem to work with gopls
+  {'v', 'gq', '<esc><cmd>lua vim.lsp.buf.range_formatting()<cr>'},
+  {'n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>'},
+  {'n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<cr>'},
+  {'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>'},
+  {'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>'},
+  {'n', 'K', '<cmd>lua vim.lsp.buf.hover()<Cr>'},
+  {'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>'},
+  {'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>'},
+  {'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>'},
+  {'n', '<leader>dq', '<cmd>lua vim.lsp.diagnostic.set_qflist()<cr>'},
+  {'n', '<leader>dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>'},
+  {'n', '<leader>ds', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>'},
+  {'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>'},
+  {'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>'},
+  -- search symbols using "f" since all my telescope mappings are prefixed with "f"
+  {'n', '<leader>fs', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]]}
+}
+
+local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   local opts = { noremap = true, silent = true }
-  -- TODO I get the error 'bad argument #2 to 'format' (number expected, got nil' and the lsp docs say that many servers do not implement this and definition() should be used instead
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- TODO explore these
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>dq', '<cmd>lua vim.lsp.diagnostic.set_qflist()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ds', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-
-  -- search symbols using "f" since all my telescope mappings are prefixed with "f"
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fs', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
+  -- TODO only add key map if the LSP has the capability see https://github.com/mfussenegger/dotfiles/blob/c878895cbda5060159eb09ec1d3e580fd407b731/vim/.config/nvim/lua/me/lsp/conf.lua#L51
+  for _, mappings in pairs(key_mappings) do
+    local mode, lhs, rhs = unpack(mappings)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+  end
 end
 
 -- nvim-cmp supports additional completion capabilities
