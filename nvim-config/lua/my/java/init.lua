b13dfa13450ea092b45c922c930e3c8a1c4e4a77
teleivo <teleivo@users.noreply.github.com>
2021-09-16 13:43:08 +0200

    nvim: add Java refactoring mappings

--- nvim-config/lua/my/java/init.lua
+++ nvim-config/lua/my/java/init.lua
@@ -1,6 +1,15 @@
 local jdtls = require('jdtls')
-local key_mappings = require('my.lsp.mappings')
+local lsp_key_mappings = require('my.lsp.mappings')
 local M = {}
+
+local key_mappings = {
+  {'n','<A-o>',[[<cmd>lua require('jdtls').organize_imports()<cr>]]},
+  {'n','crv',[[<cmd>lua require('jdtls').extract_variable()<cr>]]},
+  {'v','crv',[[<esc><cmd>lua require('jdtls').extract_variable(true)<cr>]]},
+  {'n','crc',[[<cmd>lua require('jdtls').extract_constant()<cr>]]},
+  {'v','crc',[[<esc><cmd>lua require('jdtls').extract_constant(true)<cr>]]},
+  {'v','crm',[[<esc><cmd>lua require('jdtls').extract_method(true)<cr>]]},
+}
 
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -9,6 +18,13 @@ local on_attach = function(client, bufnr)
   -- TODO only add key map if the LSP has the capability see https://github.com/mfussenegger/dotfiles/blob/c878895cbda5060159eb09ec1d3e580fd407b731/vim/.config/nvim/lua/me/lsp/conf.lua#L51
   -- find out what an LSP can with
   -- lua print(vim.inspect(vim.lsp.protocol.make_client_capabilities())
+  -- TODO make it so the mappings here override potential mappings already
+  -- defined in the LSP?
+  -- TODO concatenate lsp key mappings and the ones from here
+  for _, mappings in pairs(lsp_key_mappings) do
+    local mode, lhs, rhs = unpack(mappings)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+  end
   for _, mappings in pairs(key_mappings) do
     local mode, lhs, rhs = unpack(mappings)
     vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
