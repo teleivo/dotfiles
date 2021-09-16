160575df165dababec42611047a6ef8fd1303715
teleivo <teleivo@users.noreply.github.com>
2021-09-16 13:44:58 +0200

    nvim: rename should be under same prefix
    as other refactorings provided by for example Java jdtls

--- nvim-config/lua/my/lsp/mappings.lua
+++ nvim-config/lua/my/lsp/mappings.lua
@@ -8,13 +8,15 @@ return {
   {'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>'},
   {'n', 'K', '<cmd>lua vim.lsp.buf.hover()<Cr>'},
   {'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>'},
-  {'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>'},
+  {'n', 'crn', '<cmd>lua vim.lsp.buf.rename()<cr>'},
   {'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>'},
+
   {'n', '<leader>dq', '<cmd>lua vim.lsp.diagnostic.set_qflist()<cr>'},
   {'n', '<leader>dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>'},
   {'n', '<leader>ds', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>'},
   {'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>'},
   {'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>'},
+
   -- search symbols using "f" since all my telescope mappings are prefixed with "f"
   {'n', '<leader>fs', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]]}
 }
