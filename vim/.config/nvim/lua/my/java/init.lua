d6f887943ac81f3e5b07044db620e313235fd057
teleivo <teleivo@users.noreply.github.com>
2021-10-06 07:05:59 +0200

    move java lsp cmd into lua config

--- vim/.config/nvim/lua/my/java/init.lua
+++ vim/.config/nvim/lua/my/java/init.lua
@@ -49,7 +49,22 @@ function M.start_jdt()
     flags = {
         allow_incremental_sync = true,
     },
-    cmd = {'java-lsp', workspace_folder},
+    cmd = {
+      '/usr/lib/jvm/java-11-openjdk-amd64/bin/java',
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Dlog.protocol=true',
+      '-Dlog.level=ALL',
+      '-Xms1g',
+      '-javaagent:' .. home .. '/.local/share/lombok/lombok.jar',
+      '-jar', vim.fn.glob(home .. '/code/lsp/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_*.jar'),
+      '-configuration', home .. '/code/lsp/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux',
+      '-data', workspace_folder,
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    },
     root_dir = root_dir,
     settings = {
       java = {
