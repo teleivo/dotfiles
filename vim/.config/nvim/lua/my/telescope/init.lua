e204bf30a042422a28ad951ccc480e58190f6776
teleivo <teleivo@users.noreply.github.com>
2021-12-12 10:34:39 +0100

    nvim: configure telescope visualization
    * shorten paths so I can see filenames, especially in
    java projects
    * put results at the top which is better for my neck :joy

--- vim/.config/nvim/lua/my/telescope/init.lua
+++ vim/.config/nvim/lua/my/telescope/init.lua
@@ -1,8 +1,40 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+
+-- TODO why do I have to type i in picker Buffer for example?
 require('telescope').setup({
   defaults = {
-    layout_strategy = 'vertical',
+    path_display = { 'truncate' },
+    winblend = 0,
+    layout_strategy = 'horizontal',
+    layout_config = {
+      width = 0.95,
+      height = 0.85,
+      prompt_position = 'top',
+
+      horizontal = {
+        preview_width = function(_, cols, _)
+          if cols > 200 then
+            return math.floor(cols * 0.4)
+          else
+            return math.floor(cols * 0.6)
+          end
+        end,
+      },
+      vertical = {
+        width = 0.9,
+        height = 0.95,
+        preview_height = 0.5,
+      },
+      flex = {
+        horizontal = {
+          preview_width = 0.9,
+        },
+      },
+    },
+    selection_strategy = 'reset',
+    sorting_strategy = 'ascending',
+    scroll_strategy = 'cycle',
   },
   extensions = {
     fzf = {
