54237d6f2726915103b4e43809eb66dac04ed5f3
teleivo <teleivo@users.noreply.github.com>
2022-02-12 06:58:36 +0100

    chore: dont use tab for cmp

--- vim/.config/nvim/lua/my/cmp/init.lua
+++ vim/.config/nvim/lua/my/cmp/init.lua
@@ -32,32 +32,15 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      -- cycle forwards through items using tab
+    ['<CR>'] = function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then -- TODO why is this added in https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
-        cmp.complete()
+        cmp.confirm()
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      -- cycle backwards through items
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    end,
   },
   experimental = {
-    native_menu = false,
     ghost_text = true,
   },
   sources = {
