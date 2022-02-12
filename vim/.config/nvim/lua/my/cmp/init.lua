16fd1d8162f3ff53d1f8bb9bf392701de87003fa
teleivo <teleivo@users.noreply.github.com>
2022-02-12 07:11:59 +0100

    fix: disable cmp in comments

--- vim/.config/nvim/lua/my/cmp/init.lua
+++ vim/.config/nvim/lua/my/cmp/init.lua
@@ -2,6 +2,16 @@ local cmp = require('cmp')
 local lspkind = require('lspkind')
 
 cmp.setup({
+  enabled = function()
+    -- disable completion in comments
+    local context = require('cmp.config.context')
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == 'c' then
+      return true
+    else
+      return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+    end
+  end,
   formatting = {
     format = lspkind.cmp_format({
       menu = {
