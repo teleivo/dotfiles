f5840b456f6beb6f5e81c7298666991fcec6bd8b
teleivo <teleivo@users.noreply.github.com>
2022-02-13 06:33:38 +0100

    chore: ansible yaml schema

--- vim/.config/nvim/lua/my/lsp/init.lua
+++ vim/.config/nvim/lua/my/lsp/init.lua
@@ -92,6 +92,8 @@ require('lspconfig').yamlls.setup({
         ['https://json.schemastore.org/golangci-lint.json'] = { '.golangci.yml', '.golangci.yaml' },
         ['https://goreleaser.com/static/schema.json'] = '.goreleaser.yml',
         ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose*.yml',
+        ['https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-tasks.json'] = '/playbooks/**/tasks/*.yml',
+        ['https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-playbook.json'] = '/playbooks/*.yml',
       },
     },
   },
