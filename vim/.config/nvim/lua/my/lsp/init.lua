8130b67d12d827f531a5436d8c9e552d08e4dcea
teleivo <teleivo@users.noreply.github.com>
2022-10-19 14:48:10 +0200

    fix lua lsp path

--- vim/.config/nvim/lua/my/lsp/init.lua
+++ vim/.config/nvim/lua/my/lsp/init.lua
@@ -55,7 +55,7 @@ require('lspconfig').gopls.setup({
 })
 
 local sumneko_root_path = vim.fn.getenv('HOME') .. '/code/lsp/sumneko/lua-language-server'
-local sumneko_binary = sumneko_root_path .. '/bin/Linux/lua-language-server'
+local sumneko_binary = sumneko_root_path .. '/bin/lua-language-server'
 
 -- Make runtime files discoverable to the server
 local runtime_path = vim.split(package.path, ';')
