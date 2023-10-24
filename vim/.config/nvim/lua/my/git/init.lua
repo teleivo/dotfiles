826842557689885174f9000b0633e1d9c5433e5e
teleivo <teleivo@users.noreply.github.com>
2023-10-24 06:12:35 +0200

    align git keymap

--- vim/.config/nvim/lua/my/git/init.lua
+++ vim/.config/nvim/lua/my/git/init.lua
@@ -10,8 +10,8 @@ require('gitsigns').setup({
 
 vim.keymap.set('n', '<leader>gs', ':Git status --short<CR>')
 vim.keymap.set('n', '<leader>gd', ':Git diff<CR>')
-vim.keymap.set('n', '<leader>gds', ' :Git diff --staged<CR>')
+vim.keymap.set('n', '<leader>gds', ':Git diff --staged<CR>')
 vim.keymap.set('n', '<leader>ga', ':Git add %:p<CR>')
-vim.keymap.set('n', '<leader>gap', ' :Git add -p<CR>')
+vim.keymap.set('n', '<leader>gap', ':Git add -p<CR>')
 vim.keymap.set('n', '<leader>gc', ':Git commit -v<CR>')
 vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
