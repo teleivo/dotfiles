aff422399cd06f8fb47e363052a0b392bf8ed876
teleivo <teleivo@users.noreply.github.com>
2023-10-31 06:59:05 +0100

    improve vim command line completion

--- vim/.config/nvim/lua/my/cmp/init.lua
+++ vim/.config/nvim/lua/my/cmp/init.lua
@@ -63,10 +63,24 @@ cmp.setup({
   },
 })
 
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'cmdline', keyword_length = 2 },
+    { name = 'buffer' },
   },
+})
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' },
+      },
+    },
+  }),
 })
 
 vim.cmd([[
