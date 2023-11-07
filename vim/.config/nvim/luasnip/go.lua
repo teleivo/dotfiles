73fda691557b28cf979f6f62cc88dcff77718735
teleivo <teleivo@users.noreply.github.com>
2023-11-07 14:57:11 +0100

    initial working return statement snippet

--- vim/.config/nvim/luasnip/go.lua
+++ vim/.config/nvim/luasnip/go.lua
@@ -126,11 +126,6 @@ local function go_result_type(info)
     return t('')
   end
 
-  -- uses alternation and result; and wildcard? for a named or anonymous node
-  -- which it captures in id; I think it captures the parameter_list node
-  -- and then calls handlers['parameter_list'] which will create a table with the zero values
-  -- it even has a handler for type_identifier which I could use to check that at least one is equal
-  -- to error; maybe I can create a query
   local query = vim.treesitter.query.parse(
     'go',
     [[
@@ -156,30 +151,6 @@ local sn_return_values = function(args)
       err_name = args[1][1],
     })
   )
-end
-
-local get_function_node = function()
-  local cursor_node = ts_utils.get_node_at_cursor()
-  local scope = ts_locals.get_scope_tree(cursor_node, 0)
-
-  local function_node
-  for _, v in ipairs(scope) do
-    if function_node_types[v:type()] then
-      function_node = v
-      break
-    end
-  end
-
-  if not function_node then
-    print('Not inside of a function')
-    return nil
-  end
-
-  return function_node
-end
-
-local is_function_node = function()
-  return get_function_node() ~= nil
 end
 
 local is_function_node_returning_error = function()
@@ -216,15 +187,175 @@ local is_function_node_returning_error = function()
   return false
 end
 
-local sn_return_values_new = function(args)
-  return sn(
-    nil,
-    go_result_type({
-      index = 0,
-    })
-  )
+-- c_error creates a choice node at given jump index. Choices are either the error err_name as is,
+-- expanding on its error using a new error or wrapping it.
+local c_error_new = function(index, err_name)
+  return c(index, {
+    t(err_name),
+    sn(nil, {
+      t('fmt.Errorf("'),
+      i(1),
+      t(string.format(': %%s", %s)', err_name)),
+    }),
+    sn(nil, {
+      t('fmt.Errorf("'),
+      i(1),
+      t(string.format(': %%w", %s)', err_name)),
+    }),
+  })
 end
 
+local zero_values = {
+  byte = '0',
+  rune = '0',
+  int = '0',
+  int8 = '0',
+  int16 = '0',
+  int32 = '0',
+  int64 = '0',
+  uint = '0',
+  uint8 = '0',
+  uint16 = '0',
+  uint32 = '0',
+  uint64 = '0',
+  float32 = '0',
+  float64 = '0',
+  bool = 'false',
+  string = '""',
+  error = 'nil',
+}
+
+local transforms_new = {
+  int = function(_, _)
+    return t('0')
+  end,
+
+  bool = function(_, _)
+    return t('false')
+  end,
+
+  string = function(_, _)
+    return t([[""]])
+  end,
+
+  error = function(_, info)
+    if info then
+      info.index = info.index + 1
+      return c_error_new(info.index, info.err_name)
+    end
+
+    return t('err')
+  end,
+
+  -- Types with a "*" mean they are pointers, so return nil
+  [function(text)
+    return string.find(text, '*', 1, true) ~= nil
+  end] = function(_, _)
+    return t('nil')
+  end,
+}
+
+local transform_new = function(text, info)
+  local condition_matches = function(condition, ...)
+    if type(condition) == 'string' then
+      return condition == text
+    else
+      return condition(...)
+    end
+  end
+
+  for condition, result in pairs(transforms_new) do
+    if condition_matches(condition, text, info) then
+      return result(text, info)
+    end
+  end
+
+  return t(text)
+end
+
+local handlers_new = {
+  parameter_list = function(node, info)
+    local result = {}
+
+    local count = node:named_child_count()
+    for idx = 0, count - 1 do
+      local matching_node = node:named_child(idx)
+      local type_node = matching_node:field('type')[1]
+      table.insert(result, transforms_new(get_node_text(type_node, 0), info))
+      if idx ~= count - 1 then
+        table.insert(result, t({ ', ' }))
+      end
+    end
+    return result
+  end,
+
+  type_identifier = function(node, info)
+    local text = get_node_text(node, 0)
+    return { transform_new(text, info) }
+  end,
+}
+
+local get_function_node_at_cursor = function()
+  local cursor_node = ts_utils.get_node_at_cursor()
+  local scope = ts_locals.get_scope_tree(cursor_node, 0)
+
+  for _, v in ipairs(scope) do
+    if function_node_types[v:type()] then
+      return v
+    end
+  end
+
+  return nil
+end
+
+local go_result_types = function()
+  local function_node = get_function_node_at_cursor()
+  if not function_node then
+    error('Not inside of a function')
+  end
+
+  local query = vim.treesitter.query.parse(
+    'go',
+    [[
+      [
+        (method_declaration result: (parameter_list (parameter_declaration type: (type_identifier) @id)))
+        (function_declaration result: (parameter_list (parameter_declaration type: (type_identifier) @id)))
+        (func_literal result: (parameter_list (parameter_declaration type: (type_identifier) @id)))
+      ]
+    ]]
+  )
+
+  local result = {}
+  for _, node in query:iter_captures(function_node, 0) do
+    -- vim.print(vim.inspect(get_node_text(node, 0)))
+    table.insert(result, get_node_text(node, 0))
+  end
+
+  return result
+end
+
+local sn_result_types = function()
+  local types = go_result_types()
+  -- vim.print(vim.inspect(types))
+
+  local result = {}
+  for idx, type in pairs(types) do
+    local value = zero_values[type]
+    table.insert(result, i(idx, value))
+    if next(types, idx) then
+      table.insert(result, t({ ', ' }))
+    end
+  end
+
+  return sn(nil, result)
+end
+
+-- TODO clean up
+-- TODO create helper for an error choice node
+-- TODO how to get vars that are in scope? create a function for that
+-- TODO pass in above function with vars per type and make a choice node per result type with the
+-- zero value insert node as the first, followed by var text nodes
+-- TODO errors should get the fmt.Errorf %v and %w options in addition to the var ones
 return {
   s(
     {
@@ -237,7 +368,7 @@ return <result>
 <finish>
 ]],
       {
-        result = d(1, sn_return_values_new),
+        result = d(1, sn_result_types),
         finish = i(0),
       }
     ),
