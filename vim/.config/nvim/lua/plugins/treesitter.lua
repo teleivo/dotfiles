4cd9a45ae8860e4861f1ee7c2581482ac0a8b0e2
teleivo <teleivo@users.noreply.github.com>
2023-11-11 07:02:01 +0100

    fix treesitter setup
    not sure why passing the opts does not work

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,15 +3,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     version = false, -- last release is way too old
     build = ':TSUpdate',
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require('lazy.core.loader').add_to_rtp(plugin)
-      require('nvim-treesitter.query_predicates')
-    end,
     dependencies = {
       {
         'nvim-treesitter/nvim-treesitter-textobjects',
@@ -20,101 +11,108 @@ return {
         'nvim-treesitter/playground',
       },
     },
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'dockerfile',
-        'go',
-        'gomod',
-        'gowork',
-        'hcl',
-        'html',
-        'java',
-        'javascript',
-        'json',
-        'lua',
-        'make',
-        'query',
-        'typescript',
-        'vim',
-        'yaml',
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = 'gh',
-          node_incremental = 'gh',
-          scope_incremental = 'gH',
-          node_decremental = 'gl',
+    config = function()
+      local treesitter = require('nvim-treesitter.configs')
+
+      ---@type TSConfig
+      ---@diagnostic disable-next-line: missing-fields
+      treesitter.setup({
+        ensure_installed = {
+          'bash',
+          'c',
+          'dockerfile',
+          'go',
+          'gomod',
+          'gowork',
+          'hcl',
+          'html',
+          'java',
+          'javascript',
+          'json',
+          'lua',
+          'luadoc',
+          'make',
+          'markdown',
+          'markdown_inline',
+          'query',
+          'typescript',
+          'vim',
+          'yaml',
         },
-      },
-      textobjects = {
-        select = {
+        highlight = {
           enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
           keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@conditional.outer',
-            ['ic'] = '@conditional.inner',
-            ['ak'] = '@class.outer',
-            ['ik'] = '@class.inner',
+            init_selection = 'gh',
+            node_incremental = 'gh',
+            scope_incremental = 'gH',
+            node_decremental = 'gl',
           },
         },
-        move = {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@conditional.outer',
+              ['ic'] = '@conditional.inner',
+              ['ak'] = '@class.outer',
+              ['ik'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+        },
+        playground = {
           enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
+          disable = {},
+          updatetime = 25,
+          persist_queries = true,
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
           },
         },
-      },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25,
-        persist_queries = true,
-        keybindings = {
-          toggle_query_editor = 'o',
-          toggle_hl_groups = 'i',
-          toggle_injected_languages = 't',
-          toggle_anonymous_nodes = 'a',
-          toggle_language_display = 'I',
-          focus_language = 'f',
-          unfocus_language = 'F',
-          update = 'R',
-          goto_node = '<cr>',
-          show_help = '?',
+        query_linter = {
+          enable = true,
+          use_virtual_text = true,
+          lint_events = { 'BufWrite', 'CursorHold' },
         },
-      },
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { 'BufWrite', 'CursorHold' },
-      },
-    },
+      })
+    end,
   },
 }
