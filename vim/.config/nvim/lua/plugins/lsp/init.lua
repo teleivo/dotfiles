f6b4f1ebdb6a51f0f3ff5100205ecc5e14dd4738
teleivo <teleivo@users.noreply.github.com>
2023-11-13 07:05:51 +0100

    simplify lsp server setup

--- vim/.config/nvim/lua/plugins/lsp/init.lua
+++ vim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,9 +1,121 @@
+local servers = {
+  bashls = {},
+  jsonls = {},
+  lua_ls = {
+    Lua = {
+      workspace = {
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = 'space',
+          indent_size = '2',
+        },
+      },
+    },
+  },
+  marksman = {},
+  -- vim-go installs and updates gopls. lsp-config starts and configures the lsp
+  -- and connects neovims lsp client to it. disabled gopls usage in vim-go to get
+  -- a better/unified lsp experience across languages
+  -- available analyzers https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+  gopls = {
+    gofumpt = true,
+    analyses = {
+      nilness = true,
+      unusedparams = true,
+    },
+    staticcheck = true,
+  },
+  yamlls = {
+    yaml = {
+      format = {
+        enable = true,
+      },
+      editor = {
+        formatOnType = true,
+      },
+      schemas = {
+        ['https://json.schemastore.org/github-workflow.json'] = '/.github/workflows/*',
+        ['https://json.schemastore.org/dependabot-2.0.json'] = '/.github/dependabot.yml',
+        ['https://json.schemastore.org/golangci-lint.json'] = { '.golangci.yml', '.golangci.yaml' },
+        ['https://goreleaser.com/static/schema.json'] = '.goreleaser.yml',
+        ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose*.yml',
+        ['https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-tasks.json'] = '/playbooks/**/tasks/*.yml',
+        ['https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-playbook.json'] = '/playbooks/*.yml',
+      },
+    },
+  },
+}
+
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  -- TODO only add key map if the LSP has the capability see https://github.com/mfussenegger/dotfiles/blob/c878895cbda5060159eb09ec1d3e580fd407b731/vim/.config/nvim/lua/me/lsp/conf.lua#L51
+  -- find out what an LSP can with
+  -- lua print(vim.inspect(vim.lsp.protocol.make_client_capabilities())
+  local key_mappings = require('plugins.lsp.mappings')
+  for _, mappings in pairs(key_mappings) do
+    local mode, lhs, rhs = unpack(mappings)
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+
+  -- TODO does not work anymore
+  -- Is it because of?     Note: Usage of vim.lsp.buf.document_highlight() requires the following
+  -- highlight groups to be defined or you won't be able to see the actual
+  -- highlights. hl-LspReferenceText hl-LspReferenceRead
+  -- hl-LspReferenceWrite
+  if client.server_capabilities.documentHighlightProvider then
+    local group = vim.api.nvim_create_augroup('my_lsp', { clear = true })
+    vim.api.nvim_create_autocmd('CursorHold', {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.document_highlight()
+      end,
+      group = group,
+    })
+    vim.api.nvim_create_autocmd('CursorMoved', {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.clear_references()
+      end,
+      group = group,
+    })
+  end
+end
+-- TODO finish adding capabilities after nvim-cmp is workin again
+-- nvim-cmp supports additional completion capabilities
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- autoformat
+local group = vim.api.nvim_create_augroup('my_lua', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.lua',
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+  group = group,
+})
+
+vim.cmd([[
+  highlight! link LspDiagnosticsVirtualTextError DiagnosticError
+  highlight! link LspDiagnosticsVirtualTextWarning DiagnosticWarn
+  highlight! link LspDiagnosticsVirtualTextInfo DiagnosticInfo
+  highlight! link LspDiagnosticsVirtualTextHint DiagnosticHint
+]])
+vim.fn.sign_define('LspDiagnosticsSignError', { text = '', numhl = 'CocErrorSign' })
+vim.fn.sign_define('LspDiagnosticsSignWarning', { text = '', numhl = 'CocWarningSign' })
+vim.fn.sign_define('LspDiagnosticsSignInformation', { text = '', numhl = 'CocInfoSign' })
+vim.fn.sign_define('LspDiagnosticsSignHint', { text = '', numhl = 'CocHintSign' })
+
 return {
   {
     'neovim/nvim-lspconfig',
-    config = function()
-      require('plugins.lsp.config')
-    end,
   },
   {
     'williamboman/mason.nvim',
@@ -18,13 +130,15 @@ return {
     },
     opts = {
       automatic_installation = true,
-      ensure_installed = {
-        'lua_ls',
-        'gopls',
-        'yamlls',
-        'jsonls',
-        'bashls',
-        'marksman',
+      ensure_installed = vim.tbl_keys(servers),
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({
+            -- capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+          })
+        end,
       },
     },
   },
