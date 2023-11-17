37b955fb1192d40e73f03d383fb8f4d2618053b7
teleivo <teleivo@users.noreply.github.com>
2023-11-17 07:34:47 +0100

    add missing lsp highlight for references

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,9 +4,6 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      ---         |hl-LspReferenceText|
-      ---         |hl-LspReferenceRead|
-      ---         |hl-LspReferenceWrite|
       vim.cmd.colorscheme('dogrun')
       -- increase contrast to so whitespace is easily visible (when showing it with
       -- :set list!<CR>)
@@ -16,7 +13,12 @@ return {
         highlight SpecialKey guifg=#4a4a59
         highlight LineNr guifg=#535f98
         highlight CursorLineNr guifg=#535f98
+
+        highlight LspReferenceText guibg=#363e7f
+        highlight LspReferenceRead guibg=#363e7f
+        highlight LspReferenceWrite guibg=#363e7f
       ]])
+      -- TODO what is this for again?
       vim.fn.sign_define('DiagnosticSignError', { text = '', numhl = 'CocErrorSign' })
       vim.fn.sign_define('DiagnosticSignWarn', { text = '', numhl = 'CocWarningSign' })
       vim.fn.sign_define('DiagnosticSignInformation', { text = '', numhl = 'CocInfoSign' })
