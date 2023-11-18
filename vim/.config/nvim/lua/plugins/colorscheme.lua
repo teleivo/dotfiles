ba9f19443063fdd22202c39b500252a146f26c1f
teleivo <teleivo@users.noreply.github.com>
2023-11-18 06:50:09 +0100

    clean up colorscheme

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,20 +5,20 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       vim.cmd.colorscheme('dogrun')
-      -- increase contrast to so whitespace is easily visible (when showing it with
-      -- :set list!<CR>)
-      -- also set diagnostic highlights as vim-dogrun does not do it
       vim.cmd([[
-        highlight NonText guifg=#4a4a59
-        highlight SpecialKey guifg=#4a4a59
-        highlight LineNr guifg=#535f98
-        highlight CursorLineNr guifg=#535f98
+        " https://github.com/wadackel/vim-dogrun/blob/940814494be4adb066d4eb96409a85cb84c0bd6b/colors/dogrun.vim#L34
+        " increase contrast of LineNr by using the same values as for CursorLineNr
+        highlight LineNr guifg=#535f98 ctermfg=61 guibg=NONE ctermbg=NONE gui=NONE cterm=NONE
 
+        " until https://github.com/wadackel/vim-dogrun/issues/17 is resolved
         highlight LspReferenceText guibg=#363e7f
         highlight LspReferenceRead guibg=#363e7f
         highlight LspReferenceWrite guibg=#363e7f
       ]])
-      -- TODO what is this for again?
+      -- dogrun specifies highlight groups for the coc plugin (which I don't use) but this way I can reuse
+      -- them for the diagnostic signs
+      -- https://github.com/wadackel/vim-dogrun/blob/940814494be4adb066d4eb96409a85cb84c0bd6b/colors/dogrun.vim#L212-L215
+      -- I only color the absolute/relative line numbers of lines with diagnostics without any symbol
       vim.fn.sign_define('DiagnosticSignError', { text = '', numhl = 'CocErrorSign' })
       vim.fn.sign_define('DiagnosticSignWarn', { text = '', numhl = 'CocWarningSign' })
       vim.fn.sign_define('DiagnosticSignInformation', { text = '', numhl = 'CocInfoSign' })
