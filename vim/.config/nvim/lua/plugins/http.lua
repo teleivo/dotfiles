aada33f64e8dc8acdcd5b382e3b14b27409b012f
teleivo <teleivo@users.noreply.github.com>
2024-03-19 07:03:42 +0100

    neovim: update

--- vim/.config/nvim/lua/plugins/http.lua
+++ vim/.config/nvim/lua/plugins/http.lua
@@ -1,30 +1,38 @@
 return {
-  'rest-nvim/rest.nvim',
-  dependencies = { { 'nvim-lua/plenary.nvim' } },
-  ft = 'http',
-  config = function()
-    local rest = require('rest-nvim')
-    rest.setup({
-      result_split_horizontal = true,
-      stay_in_current_window_after_split = true,
-    })
+  {
+    'vhyrro/luarocks.nvim',
+    config = function()
+      require('luarocks').setup({})
+    end,
+  },
+  {
+    'rest-nvim/rest.nvim',
+    ft = 'http',
+    dependencies = { 'luarocks.nvim' },
+    config = function()
+      local rest = require('rest-nvim')
+      rest.setup({
+        result_split_horizontal = true,
+        stay_in_current_window_after_split = true,
+      })
 
-    vim.api.nvim_create_user_command('HttpRequest', function()
-      rest.run()
-    end, {
-      nargs = 0,
-    })
+      vim.api.nvim_create_user_command('HttpRequest', function()
+        rest.run()
+      end, {
+        nargs = 0,
+      })
 
-    vim.api.nvim_create_user_command('HttpRequestLast', function()
-      rest.last()
-    end, {
-      nargs = 0,
-    })
+      vim.api.nvim_create_user_command('HttpRequestLast', function()
+        rest.last()
+      end, {
+        nargs = 0,
+      })
 
-    vim.api.nvim_create_user_command('HttpPreview', function()
-      rest.run(true)
-    end, {
-      nargs = 0,
-    })
-  end,
+      vim.api.nvim_create_user_command('HttpPreview', function()
+        rest.run(true)
+      end, {
+        nargs = 0,
+      })
+    end,
+  },
 }
