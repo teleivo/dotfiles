6cf61d91f496b37b9b526c21ae4ed7d99368cc6d
teleivo <teleivo@users.noreply.github.com>
2024-06-16 14:51:48 +0200

    vim: use cmp in comments

--- vim/.config/nvim/lua/plugins/cmp.lua
+++ vim/.config/nvim/lua/plugins/cmp.lua
@@ -40,14 +40,7 @@ return {
             return false
           end
 
-          -- disable completion in comments
-          local context = require('cmp.config.context')
-          -- keep command mode completion enabled when cursor is in a comment
-          if vim.api.nvim_get_mode().mode == 'c' then
-            return true
-          else
-            return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
-          end
+          return true
         end,
         window = {
           completion = cmp.config.window.bordered({
