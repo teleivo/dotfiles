8dc5bf901dc9b387da74df9e723283ae50afcf23
teleivo <teleivo@users.noreply.github.com>
2024-10-07 16:32:20 +0200

    add treesitter class navigation for Java

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -66,8 +66,14 @@ return {
               ['if'] = { query = '@function.inner', desc = 'Select inner part of a function' },
               ['ac'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
               ['ic'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
-              ['ak'] = { query = '@class.outer', desc = 'Select outer part of a class or struct/interface in Go' },
-              ['ik'] = { query = '@class.inner', desc = 'Select inner part of a class or struct/interface in Go' },
+              ['ak'] = {
+                query = '@class.outer',
+                desc = 'Select outer part of a class or struct/interface in Go',
+              },
+              ['ik'] = {
+                query = '@class.inner',
+                desc = 'Select inner part of a class or struct/interface in Go',
+              },
             },
           },
           move = {
@@ -75,15 +81,19 @@ return {
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
               [']m'] = '@function.outer',
+              [']k'] = '@class.outer',
             },
             goto_next_end = {
               [']M'] = '@function.outer',
+              [']K'] = '@class.outer',
             },
             goto_previous_start = {
               ['[m'] = '@function.outer',
+              ['[k'] = '@class.outer',
             },
             goto_previous_end = {
               ['[M'] = '@function.outer',
+              ['[K'] = '@class.outer',
             },
           },
         },
