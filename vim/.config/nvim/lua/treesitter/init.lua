0d3a0272588f5033a65f0af735796c9d10b268b9
teleivo <teleivo@users.noreply.github.com>
2024-10-22 07:06:35 +0200

    wip: for refactor code action
    I did not know I could use the lsp API to trigger/filter a code action. So
    I thought I needed to use treesitter to figure out what type of node I am on
    to then trigger the specific code action like refactor.extract.variable. Turns
    out that is not needed. The code might still be useful someday though

--- vim/.config/nvim/lua/treesitter/init.lua
+++ vim/.config/nvim/lua/treesitter/init.lua
@@ -39,4 +39,35 @@ M.top_level_declaration = function()
   end
 end
 
+M.NodeVisual = function()
+  local _, _, tree = require('nvim-treesitter.ts_utils').get_root_for_position(0, 0)
+  local range = M.Get_visual_selection_range()
+  return tree:node_for_range(range)
+end
+
+M.Node = function(range)
+  local _, _, tree = require('nvim-treesitter.ts_utils').get_root_for_position(0, 0)
+  return tree:node_for_range(range)
+end
+
+M.Get_visual_selection_text = function()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
+M.Get_visual_selection_range = function()
+  local selection_start = vim.fn.getpos("'<")
+  local selection_end = vim.fn.getpos("'>")
+  return { selection_start[2] - 1, selection_start[3] - 1, selection_end[2] - 1, selection_end[3] }
+end
+
 return M
