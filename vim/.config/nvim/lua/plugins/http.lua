56ee221ef584540fdef5aeeff7acad2859754fbb
teleivo <teleivo@users.noreply.github.com>
2024-11-13 16:38:40 +0100

    fix rest-nvim dependencies

--- vim/.config/nvim/lua/plugins/http.lua
+++ vim/.config/nvim/lua/plugins/http.lua
@@ -8,7 +8,7 @@ return {
       {
         'vhyrro/luarocks.nvim',
         opts = {
-          rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
+          rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua', 'fidget.nvim' },
         },
       },
     },
