056ec7a5ac77487fda556679f20e338cb525952d
teleivo <teleivo@users.noreply.github.com>
2024-11-22 07:17:28 +0100

    getting there

--- nvim/.config/nvim/lua/go/init.lua
+++ nvim/.config/nvim/lua/go/init.lua
@@ -119,12 +119,10 @@ local function find_tests()
   return tests
 end
 
--- TODO fix reusing the same buffer
+-- TODO fix reusing the same buffer; where should I keep the state of the term_job_id?
+-- TODO navigate away from terminal buffer?
 -- TODO fix find_... funcs and type hints and null checks
 -- TODO fix deprecated API calls
--- TODO create a main Go command with subcommands like Rest plugin
--- TODO can I autocomplete the Go test {tests}? this would allow me to adjust what is passed to go
--- test like using a regex or running a specific table test
 -- TODO how can I run tests as verbose? or pass additional flags to the command?
 -- TODO allow selection of a test with vim.ui or telescope? start simple. telescope is nice as it
 -- could have a preview of the actual test on the right
@@ -137,41 +135,42 @@ local function run_test(test)
 
   local BUFNAME = 'go://tests'
   -- Check if the buffer is already open
-  local buf = vim.fn.bufnr(BUFNAME)
+  local bufnr = vim.fn.bufnr(BUFNAME)
+  local term_job_id
 
-  if buf ~= -1 then -- If buffer exists
-    -- Check if it's currently visible in a window
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-      if vim.api.nvim_win_get_buf(win) == buf then
-        -- If open, close the window
-        vim.api.nvim_win_close(win, true)
-        return
-      end
-    end
-  else
+  if bufnr == -1 then -- create terminal buffer
     -- Create a new buffer if it doesn't exist
-    buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_name(buf, BUFNAME)
+    bufnr = vim.api.nvim_create_buf(true, true)
+    -- bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(bufnr, BUFNAME)
+
+    local height = math.ceil(vim.o.lines * 0.4) -- 80% of screen height
+    local width = math.ceil(vim.o.columns * 0.4) -- 80% of screen width
+    local win = vim.api.nvim_open_win(bufnr, true, {
+      split = 'below',
+      style = 'minimal',
+      width = width,
+      height = height,
+    })
+
+    Print(bufnr)
+
+    vim.api.nvim_win_set_buf(win, bufnr)
+
+    vim.cmd('lcd ' .. vim.fn.fnameescape(find_go_mod_root()))
+
+    -- Start the terminal job in the buffer
+    vim.api.nvim_set_current_buf(bufnr)
+    term_job_id = vim.fn.termopen(vim.o.shell, {
+      on_exit = function(_, exit_code, _)
+        print('Terminal exited with code:', exit_code)
+      end,
+    })
+    vim.api.nvim_buf_set_name(bufnr, BUFNAME)
   end
 
-  local height = math.ceil(vim.o.lines * 0.4) -- 80% of screen height
-  local width = math.ceil(vim.o.columns * 0.4) -- 80% of screen width
-  local win = vim.api.nvim_open_win(buf, true, {
-    split = 'below',
-    style = 'minimal',
-    width = width,
-    height = height,
-  })
-
-  vim.api.nvim_win_set_buf(win, buf)
-
-  vim.cmd('lcd ' .. vim.fn.fnameescape(find_go_mod_root()))
-
-  -- Start a terminal in this buffer
-  vim.cmd('terminal')
-
+  Print(term_job_id)
   -- Send the command to the terminal
-  local term_job_id = vim.b.terminal_job_id
   vim.fn.chansend(term_job_id, 'go test ./... -run ' .. test .. ' \n')
 
   -- Automatically switch to insert mode for interaction
