83493ad8a77d67b59be4171d5032b845db56365c
teleivo <teleivo@users.noreply.github.com>
2024-12-23 15:11:27 +0100

    try oil.nvim

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    local detail = false
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    require('oil').setup({
+      keymaps = {
+        ['gd'] = {
+          desc = 'Toggle file detail view',
+          callback = function()
+            detail = not detail
+            if detail then
+              require('oil').set_columns({ 'icon', 'permissions', 'size', 'mtime' })
+            else
+              require('oil').set_columns({ 'icon' })
+            end
+          end,
+        },
+      },
+    })
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
