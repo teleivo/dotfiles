20df456d9f0ae9e5af04eb52f4e2cb128a8da918
teleivo <teleivo@users.noreply.github.com>
2024-12-27 14:39:18 +0100

    separate tab creation logic

--- nvim/.config/nvim/lua/my-test/diff.lua
+++ nvim/.config/nvim/lua/my-test/diff.lua
@@ -43,10 +43,15 @@ assertion_line = assertion_line:gsub('\n', '')
 local expected_string = assertion_line:match('expected: <(.-)>')
 local actual_string = assertion_line:match('but was: <(.-)>')
 
-local function create_split_windows(expected, actual, filetype)
+local function create_tab()
   vim.cmd('tabnew')
   local tabnr = vim.api.nvim_get_current_tabpage()
   vim.api.nvim_tabpage_set_var(tabnr, 'tabname', 'test-diff')
+
+  -- Opening a new tab without a file will create an empty buffer that I do not want. I don't know
+  -- of a way to disable that behavior. So for now track its handle and delete it after opening
+  -- other buffers. If I would delete it right away I would close the tab.
+  local empty_buf = vim.api.nvim_win_get_buf(0)
 
   local right_buf = vim.api.nvim_create_buf(true, true)
   vim.api.nvim_set_current_buf(right_buf)
@@ -56,26 +61,15 @@ local function create_split_windows(expected, actual, filetype)
   local left_buf = vim.api.nvim_create_buf(true, true)
   vim.api.nvim_set_current_buf(left_buf)
 
-  vim.api.nvim_buf_set_lines(left_buf, 0, -1, false, vim.split(expected, '\n'))
-  vim.bo[left_buf].filetype = filetype
-  vim.api.nvim_buf_set_name(left_buf, 'expected')
+  vim.api.nvim_buf_delete(empty_buf, { force = true })
 
-  vim.api.nvim_buf_set_lines(right_buf, 0, -1, false, vim.split(actual, '\n'))
-  vim.bo[right_buf].filetype = filetype
-  vim.api.nvim_buf_set_name(right_buf, 'actual')
-
-  vim.cmd('windo diffthis')
-
-  local bufs = {
-    left_buf,
-    right_buf,
+  return {
+    tabnr = tabnr,
+    bufs = {
+      left = left_buf,
+      right = right_buf,
+    },
   }
-
-  for _, buf in ipairs(bufs) do
-    vim.keymap.set('n', 'q', function()
-      vim.cmd('tabclose')
-    end, { buffer = buf })
-  end
 end
 
 local function is_json(str)
@@ -91,11 +85,32 @@ end
 if is_json(expected_string) and is_json(actual_string) then
   local formatted_expected = format_json(expected_string)
   local formatted_actual = format_json(actual_string)
-  create_split_windows(formatted_expected, formatted_actual, 'json')
+  local tab = create_tab()
+
+  vim.api.nvim_buf_set_lines(tab.bufs.left, 0, -1, false, vim.split(formatted_expected, '\n'))
+  vim.bo[tab.bufs.left].filetype = 'json'
+  vim.api.nvim_buf_set_name(tab.bufs.left, 'expected')
+
+  vim.api.nvim_buf_set_lines(tab.bufs.right, 0, -1, false, vim.split(formatted_actual, '\n'))
+  vim.bo[tab.bufs.right].filetype = 'json'
+  vim.api.nvim_buf_set_name(tab.bufs.right, 'actual')
+
+  vim.cmd('windo diffthis')
+
+  Print(tab)
+  for _, buf in pairs(tab.bufs) do
+    vim.keymap.set('n', 'q', function()
+      vim.cmd('tabclose')
+      for _, buf_inner in pairs(tab.bufs) do
+        vim.api.nvim_buf_delete(buf_inner, { force = true })
+      end
+    end, { buffer = buf })
+  end
 end
 
 -- TODO integrate that into the test plugin for java
 -- TODO how to extract the AssertionFailedError lines? I could add a mapping to go to the
 -- AssertionFailedError. How to then extract all lines as they are unfortunately split by newlines
 -- parse the expected/actual and feed them into this code
+-- TODO reuse the diff tab?
 -- TODO get a sample of assertEquals of a lombok class with equals implementation
