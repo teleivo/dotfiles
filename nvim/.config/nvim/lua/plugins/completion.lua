3c301fc72bd5a489d0baf093e90a7079cde86898
teleivo <teleivo@users.noreply.github.com>
2024-12-29 06:21:36 +0100

    configure avante completion

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -11,6 +11,13 @@ return {
     'saghen/blink.cmp',
     dependencies = {
       { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+      {
+        'saghen/blink.compat',
+        version = '*',
+        lazy = true,
+        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+        opts = {},
+      },
     },
     version = '*',
     ---@module 'blink.cmp'
@@ -71,8 +78,36 @@ return {
         end,
       },
       sources = {
-        default = { 'lazydev', 'luasnip', 'lsp', 'path', 'buffer', 'markdown', 'dadbod' },
+        default = {
+          'lazydev',
+          'luasnip',
+          'lsp',
+          'path',
+          'buffer',
+          'markdown',
+          'dadbod',
+          'avante_commands',
+          'avante_mentions',
+          -- 'avante_files',
+        },
         providers = {
+          avante_commands = {
+            name = 'avante_commands',
+            module = 'blink.compat.source',
+            score_offset = 90, -- show at a higher priority than lsp
+          },
+          -- TODO what is this for?
+          -- https://github.com/yetone/avante.nvim/pull/984/files
+          -- avante_files = {
+          --   name = 'avante_commands',
+          --   module = 'blink.compat.source',
+          --   score_offset = 100, -- show at a higher priority than lsp
+          -- },
+          avante_mentions = {
+            name = 'avante_mentions',
+            module = 'blink.compat.source',
+            score_offset = 1000, -- show at a higher priority than lsp
+          },
           dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
           lazydev = {
             name = 'LazyDev',
