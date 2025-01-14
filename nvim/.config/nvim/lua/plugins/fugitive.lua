7df7e4d6ac38f5ce59afe8928a6c92719f84704c
teleivo <teleivo@users.noreply.github.com>
2025-01-14 07:41:41 +0100

    improve fugitive config

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,13 +2,38 @@ return {
   {
     'tpope/vim-fugitive',
     keys = {
-      { '<leader>gg', ':Git<CR>', desc = 'Git summary' },
       { '<leader>gs', ':Git status --short<CR>', desc = 'Git status' },
       { '<leader>gd', ':Git diff<CR>', desc = 'Git diff' },
       { '<leader>gds', ':Git diff --staged<CR>', desc = 'Git diff staged' },
       { '<leader>gap', ':Git add -p<CR>', desc = 'Git add patch' },
       { '<leader>gc', ':Git commit -v<CR>', desc = 'Git commit' },
       { '<leader>gp', ':Git push<CR>', desc = 'Git push' },
+      { '<leader>gw', ':Gwrite<CR>', desc = 'Git write buffer and stage changes' },
+      {
+        '<leader>gg',
+        function()
+          local bufnr = nil
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            local bufname = vim.api.nvim_buf_get_name(buf)
+            if bufname:match('^fugitive://') then
+              bufnr = buf
+              break
+            end
+          end
+
+          if not bufnr then
+            vim.cmd.Git()
+            return
+          end
+
+          -- The fugitive buffer is kept when closing the window and thus reused on another
+          -- invocation of :Git. I am deleting the buffer for simplicity.
+          local winid = vim.fn.bufwinid(bufnr)
+          vim.api.nvim_win_close(winid, true)
+          vim.api.nvim_buf_delete(bufnr, { force = true })
+        end,
+        desc = 'Toggle git summary',
+      },
     },
     cmd = {
       'G',
