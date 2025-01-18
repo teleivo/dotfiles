1f4192af5ac21761604c019f13560439900e9da4
teleivo <teleivo@users.noreply.github.com>
2025-01-18 06:04:43 +0100

    set dot ftplugin indent config
    use after so it takes precedence over the builtin plugin.
    merge with the existing dot.lua

--- nvim/.config/nvim/after/ftplugin/dot.lua
+++ nvim/.config/nvim/after/ftplugin/dot.lua
@@ -1,3 +1,7 @@
+local width = 4
+vim.opt_local.tabstop = width
+vim.opt_local.shiftwidth = width
+vim.opt_local.softtabstop = width
 -- show the colorcolumn to double-check my https://github.com/teleivo/dot/tree/main/cmd/dotfmt
 -- honours the max column of 100
 vim.opt_local.colorcolumn = '100'
