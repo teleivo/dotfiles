54db4b4f1ded7c8d9d5e57d91d8384c8c3959040
teleivo <teleivo@users.noreply.github.com>
2025-02-02 11:47:17 +0100

    try markdown list continuation

---
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,14 @@
+-- TODO does not work for ordered lists. might also need to replace the formatpattern or so
+-- vim.opt_local.comments = { 'b:*', 'b:-', 'b:+', 'b:1', 'n:>' }
+vim.opt_local.comments = { 'b:*', 'b:-', 'b:+', 'n:>' }
+vim.opt_local.formatoptions:append('r')
+vim.opt_local.formatoptions:append('o')
+vim.keymap.set('i', '<cr>', function()
+  -- delete empty list item
+  if vim.api.nvim_get_current_line():match('^%s*[*-]%s*$') then
+    vim.api.nvim_set_current_line('')
+    return
+  end
+
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', true)
+end, { buffer = true, desc = 'Enter (removing empty list item)' })
