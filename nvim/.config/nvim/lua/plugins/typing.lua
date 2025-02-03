170c778f3261c4b61edef732838aa08e1903d9c5
teleivo <teleivo@users.noreply.github.com>
2025-02-03 08:57:32 +0100

    use on_attach in typr

--- nvim/.config/nvim/lua/plugins/typing.lua
+++ nvim/.config/nvim/lua/plugins/typing.lua
@@ -1,8 +1,12 @@
 return {
-  -- beautiful but very much a wip
   'nvzone/typr',
   dependencies = {
     'nvzone/volt',
+  },
+  opts = {
+    on_attach = function(_)
+      require('nvim-autopairs').disable()
+    end,
   },
   cmd = {
     'Typr',
