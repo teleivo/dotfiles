d0e61b76b10bcd2b18b8031b8bc13fdfa57529da
teleivo <teleivo@users.noreply.github.com>
2025-02-04 16:07:27 +0100

    merge git plugins into one file

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -61,4 +61,51 @@ return {
       'tpope/vim-rhubarb',
     },
   },
+  {
+    'lewis6991/gitsigns.nvim',
+    version = false, -- latest release is too old, pin when the next release
+    opts = {
+      preview_config = {
+        border = 'rounded',
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gitsigns.nav_hunk('next', { wrap = true })
+          end
+        end)
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gitsigns.nav_hunk('prev', { wrap = true })
+          end
+        end)
+
+        map('n', '<leader>hs', gitsigns.stage_hunk)
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
+
+        map('n', '<leader>hr', gitsigns.reset_hunk)
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
+
+        map('n', '<leader>hp', gitsigns.preview_hunk)
+        map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+      end,
+    },
+  },
 }
