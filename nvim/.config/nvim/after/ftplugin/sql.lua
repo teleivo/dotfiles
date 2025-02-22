336b33b8129c2f9dfafbbbf636972707de420e73
teleivo <teleivo@users.noreply.github.com>
2025-02-22 07:59:05 +0100

    wip db env selection

--- nvim/.config/nvim/after/ftplugin/sql.lua
+++ nvim/.config/nvim/after/ftplugin/sql.lua
@@ -1,6 +1,5 @@
 local ns = vim.api.nvim_create_namespace('my-sql')
 
--- TODO allow selecting the DB using re like I can with HTTP
 vim.keymap.set('n', '<leader>rn', function()
   local node = vim.treesitter.get_node()
   while node and node:type() ~= 'statement' do
@@ -34,3 +33,27 @@ vim.keymap.set(
   ":'<,'>DB<CR>",
   { buffer = true, desc = 'Run visually selected SQL' }
 )
+-- TODO allow selecting the DB using re like I can with HTTP
+vim.keymap.set('n', '<leader>re', function()
+  local env_pattern = '.*%.env.*'
+  local start = vim.api.nvim_buf_get_name(0)
+  local git_dir = vim.fs.root(0, '.git')
+  -- vim.fs.find stop is exclusive meaning the stop dir will not be searched
+  local stop = vim.fs.dirname(git_dir)
+  local env_files = vim.fs.find(function(name)
+    return name:match(env_pattern)
+  end, {
+    limit = math.huge,
+    type = 'file',
+    path = start,
+    stop = stop,
+    upward = true,
+  })
+  vim.ui.select(env_files, {
+    prompt = 'Select .env file to read DB connection from:',
+  }, function(choice)
+    Print(choice)
+  end)
+  -- parse .env for DB vars
+  -- allow selecting DB if multiple or select directly
+end, { buffer = true, desc = 'Select .env file for running SQL using vim-dadbod' })
