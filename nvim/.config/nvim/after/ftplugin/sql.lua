01d1a5ee9a6b57f9f32dc9a27648fd66894f8309
teleivo <teleivo@users.noreply.github.com>
2025-02-22 07:16:20 +0100

    allow executing nearest SQL statement

--- nvim/.config/nvim/after/ftplugin/sql.lua
+++ nvim/.config/nvim/after/ftplugin/sql.lua
@@ -1,5 +1,16 @@
 -- TODO implement nearest using treesitter
-vim.keymap.set('n', '<leader>rn', ':%DB<CR>', { buffer = true, desc = 'Run nearest SQL statement' })
+vim.keymap.set('n', '<leader>rn', function()
+  local node = vim.treesitter.get_node()
+  while node and node:type() ~= 'statement' do
+    node = node:parent()
+  end
+  if node then
+    local start_row, _, end_row, _ = node:range()
+    vim.cmd(string.format('%d,%dDB', start_row + 1, end_row + 1))
+  else
+    vim.notify('No SQL statement found', vim.log.levels.WARN)
+  end
+end, { buffer = true, desc = 'Run nearest SQL statement' })
 vim.keymap.set('n', '<leader>rr', ':%DB<CR>', { buffer = true, desc = 'Run current SQL file' })
 vim.keymap.set(
   'v',
