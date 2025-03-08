dcd5842724d5c530906da1f1884d53ff7787c4c7
teleivo <teleivo@users.noreply.github.com>
2025-03-08 06:21:39 +0100

    try using completion on demand

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -19,17 +19,22 @@ return {
         return vim.bo.buftype ~= 'prompt' and vim.b.completion ~= false
       end,
       keymap = {
-        preset = 'default',
-        ['<C-space>'] = {}, -- disable
-        ['<C-e>'] = {
-          function(cmp)
-            if cmp.is_visible() then
-              cmp.hide()
-            else
-              cmp.show()
-            end
-          end,
-        },
+        -- Using parts of the preset
+        -- https://cmp.saghen.dev/configuration/keymap.html#default
+        -- Completion is disabled by default and opened with C-n when needed
+        ['<C-e>'] = { 'cancel' }, -- default is 'hide'
+        ['<C-y>'] = { 'select_and_accept' },
+
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' }, -- default is without 'show'
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
       appearance = {
         use_nvim_cmp_as_default = true,
@@ -37,6 +42,7 @@ return {
       },
       completion = {
         menu = {
+          auto_show = false, -- completion feels distracting try only using it when needed
           border = 'rounded',
           winhighlight = 'Normal:Normal,FloatBorder:Comment,CursorLine:Visual,Search:None',
           draw = {
@@ -47,6 +53,8 @@ return {
             },
           },
         },
+        list = { selection = { preselect = true, auto_insert = true } },
+        ghost_text = { enabled = false },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 500,
@@ -55,7 +63,6 @@ return {
             winhighlight = 'Normal:Normal,FloatBorder:Comment,CursorLine:Visual,Search:None',
           },
         },
-        ghost_text = { enabled = true },
       },
       signature = {
         enabled = true,
