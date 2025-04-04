8d3cb703058b5382c67900cd14be230942c78316
teleivo <teleivo@users.noreply.github.com>
2025-04-04 17:08:20 +0200

    add session picker

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -9,6 +9,33 @@ return {
   -- mnemonic for keymap is 'edit' ('s' which would be great for session is already taken for snippets)
   keys = {
     {
+      '<leader>fz',
+      function()
+        local actions_state = require('telescope.actions.state')
+        local actions = require('telescope.actions')
+
+        local project_name = require('git').get_git_project_name()
+        -- TODO disable multi select
+        local opts = {
+          prompt_title = 'Find and restore a VIM session',
+          cwd = session_dir,
+          default_text = project_name,
+          attach_mappings = function(_, map)
+            map({ 'i', 'n' }, '<CR>', function(prompt_bufnr)
+              local selected_entry = actions_state.get_selected_entry()
+              vim.api.nvim_feedkeys(':source ' .. session_dir .. selected_entry[1], 'n', false)
+              actions.close(prompt_bufnr)
+            end)
+
+            -- do not trigger default mappings
+            return false
+          end,
+        }
+        require('telescope.builtin').find_files(opts)
+      end,
+      desc = 'Find and restore a VIM session',
+    },
+    {
       '<leader>es',
       function()
         local project_name = require('git').get_git_project_name()
