792ea267ebc0fb61d7bf904dcc27b406914831d5
teleivo <teleivo@users.noreply.github.com>
2025-04-05 10:23:47 +0200

    add rerun last lua chunk

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -4,13 +4,19 @@ vim.opt_local.shiftwidth = width
 vim.opt_local.softtabstop = width
 vim.opt_local.expandtab = true
 
--- TODO do I want to rerun the last chunk? I have it in a function already?
--- or rerun last range is more what I want. if its range take care of that range not existing
--- anymore
-local run = function(code)
-  code = table.concat(code, '\n')
+-- TODO prefix notify with some plugin prefix: extract it into a func
 
-  local fn, err = loadstring(code)
+-- Run a chunk of lua code and capture its calls to print() and return values in a scratch buffer in
+-- a preview window.
+--
+-- Using this instead of ':lua', ':=' or ':luafile' as these show only the calls to print() in the
+-- :messages which is hard to interact with. This allows me to interact with the output like with
+-- any other buffer text.
+--- @param chunk string[] Array of lines, or empty array for unloaded buffer.
+local run = function(chunk)
+  local chunk_str = table.concat(chunk, '\n')
+
+  local fn, err = loadstring(chunk_str)
   if err then
     vim.notify("Error loading Lua chunk '" .. err .. "'", vim.log.levels.ERROR)
     return
@@ -88,12 +94,41 @@ local run = function(code)
   })
 end
 
--- using ':%lua' instead of ':luafile' so it also works on a scratch buffer
+local visual_start
+local visual_end
+
 vim.keymap.set('n', '<leader>rr', function()
-  local code = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  run(code)
+  local chunk = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  run(chunk)
 end, { buffer = true, desc = 'Run current Lua file' })
+
 vim.keymap.set('v', '<leader>rr', function()
-  local code = vim.fn.getregion(vim.fn.getpos('.'), vim.fn.getpos('v'), { type = vim.fn.mode() })
-  run(code)
-end, { buffer = true, desc = 'Run visually selected Lua code' })
+  visual_start = vim.fn.getpos('.')
+  visual_end = vim.fn.getpos('v')
+  local chunk = vim.fn.getregion(visual_start, visual_end, { type = vim.fn.mode() })
+  run(chunk)
+end, { buffer = true, desc = 'Run visually selected Lua chunk' })
+
+-- This runs the last visually selected Lua chunk. I could also capture the last parsed chunk. This
+-- approach is a bit more flexible as it allows me to change the chunk and quickly rerun it as long
+-- as the region does not change.
+vim.keymap.set('n', '<leader>rl', function()
+  if not visual_start or not visual_end then
+    vim.notify('No previous visual selection of a Lua chunk', vim.log.levels.INFO)
+    return
+  end
+
+  local ok, chunk = pcall(function()
+    return vim.fn.getregion(visual_start, visual_end, { type = vim.fn.visualmode() })
+  end)
+  if not ok then
+    vim.notify('Previous visually selected region is no longer valid', vim.log.levels.ERROR)
+    return
+  end
+  if not chunk then
+    vim.notify('No Lua chunk selected', vim.log.levels.ERROR)
+    return
+  end
+
+  run(chunk)
+end, { buffer = true, desc = 'Re-run last visually selected Lua chunk' })
