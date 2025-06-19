1851b8bad46f4a5fbeb0bccb83a0c4eb885a8763
teleivo <teleivo@users.noreply.github.com>
2025-06-19 09:13:28 +0200

    add cool filetype

---
+++ nvim/.config/nvim/after/ftplugin/cool.lua
@@ -0,0 +1,7 @@
+local width = 4
+vim.opt_local.tabstop = width
+vim.opt_local.shiftwidth = width
+vim.opt_local.softtabstop = width
+
+-- use line comments only
+vim.opt_local.commentstring = '--%s'
