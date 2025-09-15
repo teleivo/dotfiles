189c1ba0050bb8d7432d70eb1a5ec69b782f91e8
teleivo <teleivo@users.noreply.github.com>
2025-09-15 14:07:32 +0200

    remove bluethooth button

--- pipewire/.config/wireplumber/policy.lua.d/52-avantree-default.lua
+++
@@ -1,8 +0,0 @@
--- Set Avantree C81 as preferred default devices
--- Ensures dictation and audio routing work consistently
-
-default_policy.policy = {
-  -- Prefer Avantree devices for audio routing
-  ["device.routes.default-sink-volume"] = 1.0,
-  ["device.routes.default-source-volume"] = 1.0,
-}
