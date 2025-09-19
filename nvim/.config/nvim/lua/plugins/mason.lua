e5a7c95f1420d377e91af167ce2579e6f8dabd18
teleivo <teleivo@users.noreply.github.com>
2025-09-19 08:43:12 +0200

    pin tools

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -25,24 +25,24 @@ return {
 			-- LSP names and repos can also be found in
 			-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 			ensure_installed = {
-				'codespell',
-				'delve',
+				{ 'codespell', version = '2.4.1' },
+				'delve', -- binary release, no version pinning available
 				{ 'golangci-lint', version = 'v1.64.8' },
 				-- keep in sync with version used in ~/code/dhis2/core/dhis-2/pom.xml
 				{ 'google-java-format', version = 'v1.24.0' },
-				'shellcheck',
-				'sqlfmt',
+				'shellcheck', -- binary release, no version pinning available
+				'sqlfmt', -- binary release, no version pinning available
 				-- TODO setup
 				-- 'postgrestools', -- https://github.com/supabase-community/postgres-language-server
-				'stylua',
+				'stylua', -- binary release, no version pinning available
 				'gopls', -- https://github.com/golang/tools/tree/master/gopls
-				'yamlls', -- https://github.com/redhat-developer/yaml-language-server
-				'bashls', -- https://github.com/bash-lsp/bash-language-server
+				{ 'yamlls', version = '1.17.0' }, -- https://github.com/redhat-developer/yaml-language-server
+				{ 'bashls', version = '5.4.3' }, -- https://github.com/bash-lsp/bash-language-server
 				'denols', -- https://github.com/denoland/deno
-				'jsonls', -- extracted from https://github.com/microsoft/vscode/tree/main/extensions/json-language-features/server
+				{ 'jsonls', version = '4.10.0' }, -- extracted from https://github.com/microsoft/vscode/tree/main/extensions/json-language-features/server
 				'lua_ls', -- https://github.com/LuaLS/lua-language-server
 				'ruff', -- https://github.com/astral-sh/ruff
-				'marksman', -- https://github.com/artempyanykh/marksman
+				{ 'marksman', version = '2024-12-18' }, -- https://github.com/artempyanykh/marksman
 				-- 'luacheck', -- TODO fails to install
 			},
 			auto_update = false, -- update manually so I don't have to deal with any bugs in new versions when I don't have time
