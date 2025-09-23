83d5609a0d7c7c6e7e82f9d8bc1589660aa25b4d
teleivo <teleivo@users.noreply.github.com>
2025-09-23 04:48:22 +0200

    update nvim-treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    version = false, -- last release is way too old
+    branch = 'main', -- using main branch as recommended
     build = ':TSUpdate',
     dependencies = {
       {
@@ -111,29 +111,6 @@ return {
               ['[K'] = '@class.outer',
             },
           },
-        },
-        playground = {
-          enable = true,
-          disable = {},
-          updatetime = 25,
-          persist_queries = true,
-          keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<cr>',
-            show_help = '?',
-          },
-        },
-        query_linter = {
-          enable = true,
-          use_virtual_text = true,
-          lint_events = { 'BufWrite', 'CursorHold' },
         },
       })
     end,
