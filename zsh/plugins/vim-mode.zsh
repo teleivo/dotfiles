a21ba7c01ba2fa837b8e910a9f723db52be453b1
teleivo <teleivo@users.noreply.github.com>
2026-02-08 13:26:25 +0100

    Remove duplicate surround widget registration
    The vim-mode plugin already registers surround widgets (delete-surround,
    change-surround, add-surround) at lines 264-271. The identical
    registration at the bottom of the file was redundant.

--- zsh/plugins/vim-mode.zsh
+++ zsh/plugins/vim-mode.zsh
@@ -702,13 +702,3 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
-
-# Emulation of vim-surround
-autoload -Uz surround
-zle -N delete-surround surround
-zle -N add-surround surround
-zle -N change-surround surround
-bindkey -M vicmd cs change-surround
-bindkey -M vicmd ds delete-surround
-bindkey -M vicmd ys add-surround
-bindkey -M visual S add-surround
