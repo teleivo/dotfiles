3e11c9c65b6cac30e86909bf3aef64483b4643e7
teleivo <teleivo@users.noreply.github.com>
2026-02-08 09:54:01 +0100

    support multi-line commands in history search

--- fzf-scripts/dot-fzf-scripts/history.zsh
+++ fzf-scripts/dot-fzf-scripts/history.zsh
@@ -16,14 +16,18 @@ if [[ $# -gt 0 ]]; then
   case "$1" in
     dir)
       local cwd="${_FZF_HIST_CWD//\'/\'\'}"
-      sqlite3 "$__h_db" "select command from history where cwd = '$cwd' and deleted_at is null group by command order by max(timestamp) desc"
+      sqlite3 -cmd '.separator "" "\x1e"' "$__h_db" \
+        "select command from history where cwd = '$cwd' and deleted_at is null group by command order by max(timestamp) desc" \
+        | tr '\036' '\0'
       ;;
     global)
-      sqlite3 "$__h_db" "select command from history where deleted_at is null group by command order by max(timestamp) desc"
+      sqlite3 -cmd '.separator "" "\x1e"' "$__h_db" \
+        "select command from history where deleted_at is null group by command order by max(timestamp) desc" \
+        | tr '\036' '\0'
       ;;
     delete)
       local cmd
-      IFS= read -r cmd
+      cmd=$(cat)
       local escaped="${cmd//\'/\'\'}"
       sqlite3 "$__h_db" "update history set deleted_at = datetime('now') where command = '$escaped' and deleted_at is null"
       ;;
@@ -40,10 +44,11 @@ _fzf_history_search() {
     --border-label "history [dir: $short]" \
     --header 'ctrl-r: toggle dir/global  |  ctrl-y: copy  |  alt-d: delete  |  enter: execute  |  tab: edit' \
     --no-multi \
+    --read0 \
     --expect=tab \
     --bind 'start:reload:zsh '"$__h"' dir' \
     --bind 'ctrl-y:execute-silent(echo -n {} | xsel --clipboard)+abort' \
-    --bind 'alt-d:execute-silent(echo {} | zsh '"$__h"' delete)+transform:[[ $FZF_BORDER_LABEL =~ dir ]] &&
+    --bind 'alt-d:execute-silent(echo -n {} | zsh '"$__h"' delete)+transform:[[ $FZF_BORDER_LABEL =~ dir ]] &&
       echo "reload:zsh '"$__h"' dir" ||
       echo "reload:zsh '"$__h"' global"' \
     --bind 'ctrl-r:transform:[[ $FZF_BORDER_LABEL =~ dir ]] &&
@@ -54,8 +59,8 @@ _fzf_history_search() {
 fzf-history-widget() {
   local output key cmd
   output=$(_fzf_history_search)
-  key=$(head -1 <<< "$output")
-  cmd=$(sed -n '2p' <<< "$output")
+  key="${output%%$'\n'*}"
+  cmd="${output#*$'\n'}"
 
   if [[ -n "$cmd" ]]; then
     LBUFFER="$cmd"
